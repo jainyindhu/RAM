84d81e8c5825fe77688b71594e609ec0331e6de4
jainyindhu <jainyindhu1617@gmail.com>
2026-08-11 08:32:39 +0530

    Create ram.v

---
+++ ram.v
@@ -0,0 +1,22 @@
+module ram (
+    input wire clk,
+    input wire we,
+    input wire [2:0] address,
+    input wire [7:0] data_in,
+    output reg [7:0] data_out
+);
+
+reg [7:0] memory [0:7];
+
+// Write and read operation
+always @(posedge clk) begin
+    if (we)
+        memory[address] <= data_in;
+end
+
+// Read operation
+always @(*) begin
+    data_out = memory[address];
+end
+
+endmodule
